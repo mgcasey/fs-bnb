bb0b87254b074c002332b66a764d517ffb0288d1
Michael Casey <meghancasey@Michaels-MacBook-Pro.local>
2019-05-28 19:52:52 +0100

    extra buttons added, style still needed

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+constructor(private navCtrl: NavController ) {}
 
+  navToExplore(){
+    this.navCtrl.navigateForward('tabs');
+  }
   ngOnInit() {
   }
 
