9f9184aca2e5b3dc6225c0a305cf1acc8cf105ef
Michael Casey <meghancasey@Michaels-MacBook-Pro.local>
2019-06-02 20:52:40 +0100

    styling work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
   { path: 'tab5', loadChildren: './tab5/tab5.module#Tab5PageModule' },
   { path: 'rental1', loadChildren: './rental1/rental1.module#Rental1PageModule' },
-  { path: 'property-details', loadChildren: './property-details/property-details.module#PropertyDetailsPageModule' }
+  { path: 'property-details', loadChildren: './property-details/property-details.module#PropertyDetailsPageModule' },
+  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' }
 ];
 @NgModule({
   imports: [
